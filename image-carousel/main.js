fd612da4d1e98f2576ed357bf5b73bb19e1035de
SHOIYAN <shoiyan9@gmail.com>
2025-12-21 13:09:46 +0300

    feat: add image-carousel logic

--- image-carousel/main.js
+++ image-carousel/main.js
@@ -1,26 +1,63 @@
-'use strict';
+"use strict";
 
-const leftArrow = document.querySelector('.left-arrow');
-const rightArrow = document.querySelector('.right-arrow');
-const cardList = document.querySelector('.card-list');
-const positionValue = cardList.style.transform
+const leftArrow = document.querySelector(".left-arrow button");
+const rightArrow = document.querySelector(".right-arrow button");
+const cardList = document.querySelector(".card-list");
+const dots = document.querySelectorAll(".card-dots li");
+const positionValue = cardList.style.transform;
 
-let val = 0;
+let xTranslation = 0;
+const cardWidth = 400;
+const noOfCards = document.querySelectorAll(".card-item").length;
+const firstCard = 0;
+const lastCard = -(cardWidth * (noOfCards - 1));
 
-function getTranslateX(element) {
-  const style = window.getComputedStyle(element);
-  const matrix = new DOMMatrix(style.transform);
-  return matrix.m41;
+dots.forEach((dot) => {
+  dot.addEventListener("click", () => {
+    const slideIndex = Number(dot.dataset.slide);
+    xTranslation = -(slideIndex * cardWidth);
+    updateCarousel();
+  });
+});
+
+function updateDots() {
+  dots.forEach((dot) => dot.classList.remove("active"));
+
+  const activeIndex = Math.abs(xTranslation) / cardWidth;
+  if (dots[activeIndex]) {
+    dots[activeIndex].classList.add("active");
+  }
 }
 
-leftArrow.addEventListener('click',() => {
-    val = getTranslateX(cardList);
-    if (val === 0) return;
-    cardList.style.transform = `translateX(${val + 400}px)`;
-    
-})
-rightArrow.addEventListener('click',() => {
-    val = getTranslateX(cardList);
-    if (val === -1600) return;
-    cardList.style.transform = `translateX(${val - 400}px)`;
-})
+function updateCarousel() {
+  cardList.style.transform = `translateX(${xTranslation}px)`;
+  updateDots();
+}
+
+leftArrow.addEventListener("click", () => {
+  if (xTranslation === firstCard) {
+    xTranslation = lastCard;
+  } else {
+    xTranslation += cardWidth;
+  }
+  updateCarousel();
+});
+rightArrow.addEventListener("click", () => {
+  if (xTranslation === lastCard) {
+    xTranslation = firstCard;
+  } else {
+    xTranslation -= cardWidth;
+  }
+  updateCarousel();
+});
+
+setInterval(() => {
+  if (xTranslation === lastCard) {
+    xTranslation = firstCard;
+  } else {
+    xTranslation -= cardWidth;
+  }
+  updateCarousel();
+}, 5000);
+
+updateDots();
